3dd4296153650acd66b2a089d33594d5e8680beb
timchen1997 <chen_tr@126.com>
2017-06-27 02:30:42 +0800

    管理端要求

---
+++ admin/letmeknow-admin/letmeknow-admin/GeneralSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letmeknow_admin
+{
+    class GeneralSetting
+    {
+        public static string host = "http://localhost";
+    }
+}
