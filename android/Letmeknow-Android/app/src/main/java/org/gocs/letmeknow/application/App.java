6fab482227eb23bd08dfbebb8be1112729e87141
dynamicheart <yangjianbang112@gmail.com>
2017-07-03 09:57:05 +0800

    fix gradle

--- android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/application/App.java
+++ android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/application/App.java
@@ -1,10 +1,27 @@
 package org.gocs.letmeknow.application;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 /**
  * Created by dynamicheart on 6/26/2017.
  */
 
 public class App extends Application {
+
+    @SuppressWarnings("StaticFieldLeak")
+    private static Context context;
+
+    final private static String SHARED_PREFS_COOKIE = "cookie";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+
+    public static SharedPreferences getSharedPrefsCookie() {
+        return context.getSharedPreferences(SHARED_PREFS_COOKIE, Context.MODE_PRIVATE);
+    }
 }
