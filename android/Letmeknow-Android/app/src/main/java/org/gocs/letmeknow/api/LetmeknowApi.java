42493f1cc165b458744ecfb70991c886a81c874a
rebas <154587260@qq.com>
2017-07-04 13:44:37 +0800

    add network package

---
+++ android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/api/LetmeknowApi.java
@@ -0,0 +1,8 @@
+package org.gocs.letmeknow.api;
+
+/**
+ * Created by lenovo on 2017/7/4.
+ */
+
+public interface LetmeknowApi {
+}
---
+++ android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/network/RetrofitClient.java
@@ -0,0 +1,8 @@
+package org.gocs.letmeknow.network;
+
+/**
+ * Created by lenovo on 2017/7/4.
+ */
+
+public class RetrofitClient {
+}
