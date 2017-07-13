027dd63f96e093f3a3bd2113a6a911255da866cd
dynamicheart <yangjianbang112@gmail.com>
2017-07-13 10:11:57 +0800

    add recycle view

--- android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/fragment/CircleFragment.java
+++ android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/fragment/CircleFragment.java
@@ -2,12 +2,16 @@ package org.gocs.letmeknow.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.gocs.letmeknow.R;
+import org.gocs.letmeknow.network.RetrofitClient;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -15,11 +19,21 @@ import butterknife.ButterKnife;
  */
 
 public class CircleFragment extends BaseFragment {
+    @BindView(R.id.recyclerview_circle_list)
+    RecyclerView recyclerViewCircleList;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_circle, container, false);
         ButterKnife.bind(this, view);
+        setupRecyclerView();
         return view;
+    }
+
+    private void setupRecyclerView(){
+        RetrofitClient.getService();
+        recyclerViewCircleList.setLayoutManager(new LinearLayoutManager(recyclerViewCircleList.getContext()));
+
     }
 }
