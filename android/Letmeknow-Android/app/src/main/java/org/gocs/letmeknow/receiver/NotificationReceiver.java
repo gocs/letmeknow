7e356ef73f1b96d60d6efc5755fd489e8df5e331
rebas <154587260@qq.com>
2017-07-19 16:08:25 +0800

    add some features

--- android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/receiver/NotificationReceiver.java
+++ android/Letmeknow-Android/app/src/main/java/org/gocs/letmeknow/receiver/NotificationReceiver.java
@@ -9,6 +9,7 @@ import android.support.v4.app.NotificationCompat;
 
 import com.avos.avoscloud.AVOSCloud;
 
+import org.gocs.letmeknow.R;
 import org.gocs.letmeknow.activity.NotificationDetailActivity;
 import org.json.JSONObject;
 
@@ -20,16 +21,20 @@ public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         try{
-            if (intent.getAction().equals("com.gocs.letmeknow.action.NOTIFICATION")){
+
                 JSONObject jsonObject = new JSONObject(intent.getExtras().getString("com.avos.avoscloud.Data"));
 
                 Intent resultIntent = new Intent(AVOSCloud.applicationContext, NotificationDetailActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(AVOSCloud.applicationContext, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(AVOSCloud.applicationContext)
+                        .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("呱呱呱")
                         .setContentText("沟里果酱生死蚁")
                         .setTicker("sdf");
+
+                builder.setContentIntent(pendingIntent);
+                builder.setAutoCancel(true);
 
                 int notificationId = 10086;
                 NotificationManager notificationManager =
@@ -37,7 +42,7 @@ public class NotificationReceiver extends BroadcastReceiver {
                         .getSystemService(Context.NOTIFICATION_SERVICE);
 
                 notificationManager.notify(notificationId, builder.build());
-            }
+
 
         }catch (Exception ignore){
 
